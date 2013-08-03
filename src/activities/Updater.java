9fa0783d4488c2a41a188f3aac35fdfa958c03b9
jlcvp <jlcvp@cin.ufpe.br>
2013-08-03 07:38:04 -0300

    Mudanças de base, ainda não usadas/testadas

---
+++ src/activities/Updater.java
@@ -0,0 +1,178 @@
+package activities;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.lolchampionsvoices.R;
+import com.lolchampionsvoices.R.layout;
+import com.lolchampionsvoices.R.menu;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Updater extends Activity {
+
+	 // button to show progress dialog
+    Button btnShowProgress;
+ 
+    // Progress Dialog
+    private ProgressDialog pDialog;
+    ImageView my_image;
+    // Progress dialog type (0 - for Horizontal progress bar)
+    public static final int progress_bar_type = 0;
+ 
+    // File url to download
+    private static String file_url = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Syndra_0.jpg";
+ 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_updater);
+		
+		 // show progress bar button
+        btnShowProgress = (Button) findViewById(R.id.btnProgressBar);
+        // Image view to show image after downloading
+        my_image = (ImageView) findViewById(R.id.my_image);
+        /**
+         * Show Progress bar click event
+         * */
+        btnShowProgress.setOnClickListener(new View.OnClickListener() {
+ 
+            @Override
+            public void onClick(View v) {
+                // starting new Async Task
+                new DownloadFileFromURL().execute(file_url);
+            }
+        });
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.updater, menu);
+		return true;
+	}
+	
+	
+	/**
+     * Showing Dialog
+     * */
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        case progress_bar_type: // we set this to 0
+            pDialog = new ProgressDialog(this);
+            pDialog.setMessage("Downloading file. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setMax(100);
+            pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            pDialog.setCancelable(true);
+            pDialog.show();
+            return pDialog;
+        default:
+            return null;
+        }
+    }
+ 
+    /**
+     * Background Async Task to download file
+     * */
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+ 
+        /**
+         * Before starting background thread
+         * Show Progress Bar Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showDialog(progress_bar_type);
+        }
+ 
+        /**
+         * Downloading file in background thread
+         * */
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection conection = url.openConnection();
+                conection.connect();
+                // this will be useful so that you can show a tipical 0-100% progress bar
+                int lenghtOfFile = conection.getContentLength();
+ 
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+ 
+                // Output stream
+                OutputStream output = new FileOutputStream("/sdcard/downloadedfile.jpg");
+ 
+                byte data[] = new byte[1024];
+ 
+                long total = 0;
+ 
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress(""+(int)((total*100)/lenghtOfFile));
+ 
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+ 
+                // flushing output
+                output.flush();
+ 
+                // closing streams
+                output.close();
+                input.close();
+ 
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+ 
+            return null;
+        }
+ 
+        /**
+         * Updating progress bar
+         * */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            pDialog.setProgress(Integer.parseInt(progress[0]));
+       }
+ 
+        /**
+         * After completing background task
+         * Dismiss the progress dialog
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after the file was downloaded
+            dismissDialog(progress_bar_type);
+ 
+            // Displaying downloaded image into image view
+            // Reading image path from sdcard
+            String imagePath = Environment.getExternalStorageDirectory().toString() + "/downloadedfile.jpg";
+            // setting downloaded into image view
+            my_image.setImageDrawable(Drawable.createFromPath(imagePath));
+        }
+ 
+    }
+
+}
