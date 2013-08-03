677c7e15b3939c53438173cd9c4ff2825e527355
jlcvp <jlcvp@cin.ufpe.br>
2013-08-03 10:26:53 -0300

    Muito lindo, verificando arquivos locais e se nao tiver ok, baixa arquivos remotos. tudo com progressDialog de 2 tipos (circle e barra de progresso)
    vou até dar merge na master

--- src/activities/Updater.java
+++ src/activities/Updater.java
@@ -31,6 +31,7 @@ public class Updater extends Activity {
     ImageView my_image;
     // Progress dialog type (0 - for Horizontal progress bar)
     public static final int progress_bar_type = 0;
+    public static final int progress_circle_type = 1;
  
     // File url to download
     private static String file_url = "http://leu.lemanolos.com/thumbs.zip";
@@ -75,13 +76,23 @@ public class Updater extends Activity {
         switch (id) {
         case progress_bar_type: // we set this to 0
             pDialog = new ProgressDialog(this);
-            pDialog.setMessage("Downloading media files. Please wait...");
+            pDialog.setMessage("Baixando arquivos de mídia");
             pDialog.setIndeterminate(false);
             pDialog.setMax(100);
             pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
             pDialog.setCancelable(true);
             pDialog.show();
             return pDialog;
+            
+        case progress_circle_type:
+        	pDialog = new ProgressDialog(this);
+            pDialog.setMessage("Verificando consistência de arquivos...");
+            pDialog.setIndeterminate(true);
+            pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            pDialog.setCancelable(true);
+            pDialog.show();
+            return pDialog;
+            
         default:
             return null;
         }
@@ -92,14 +103,17 @@ public class Updater extends Activity {
      * */
     class DownloadFileFromURL extends AsyncTask<String, String, String> {
  
+    	int dialogType;
         /**
          * Before starting background thread
          * Show Progress Bar Dialog
          * */
-        @Override
+        @SuppressWarnings("deprecation")
+		@Override
         protected void onPreExecute() {
             super.onPreExecute();
-            showDialog(progress_bar_type);
+            dialogType = 0;
+            showDialog(progress_circle_type);
         }
  
         /**
@@ -113,39 +127,56 @@ public class Updater extends Activity {
                 URLConnection conection = url.openConnection();
                 conection.connect();
                 // this will be useful so that you can show a tipical 0-100% progress bar
-                int lenghtOfFile = conection.getContentLength();
+                int lengthOfRemoteFile = conection.getContentLength();
+                Log.i("UpdaterActivity", "Length of remote file: " + lengthOfRemoteFile);
  
                 // download the file
                 InputStream input = new BufferedInputStream(url.openStream(), 8192);
  
                 // Output stream
-                File fileout = new File(getExternalFilesDir(null),"thumbs.zip"); 
-                OutputStream output = new FileOutputStream(fileout);
- 
-                byte data[] = new byte[1024];
- 
-                long total = 0;
- 
-                while ((count = input.read(data)) != -1) {
-                    total += count;
-                    // publishing the progress....
-                    // After this onProgressUpdate will be called
-                    publishProgress(""+(int)((total*100)/lenghtOfFile));
- 
-                    // writing data to file
-                    output.write(data, 0, count);
+                File localFile = new File(getExternalFilesDir(null),"thumbs.zip"); 
+                Log.i("UpdaterActivity", "Length of Local file: " + localFile.length());
+                
+                if(!localFile.exists() || localFile.length() != lengthOfRemoteFile)
+                {
+
+                	Log.i("UpdaterActivity","Arquivo local não existe ou possui" +
+                			"um tamanho diferente do esperado, baixando de novo");
+
+                	publishProgress("baixando");	
+
+                	OutputStream output = new FileOutputStream(localFile);
+
+                	byte data[] = new byte[1024];
+
+                	long total = 0;
+
+                	while ((count = input.read(data)) != -1) {
+                		total += count;
+                		// publishing the progress....
+                		// After this onProgressUpdate will be called
+                		publishProgress("progresso",""+(int)((total*100)/lengthOfRemoteFile));
+
+                		// writing data to file
+                		output.write(data, 0, count);
+                	}
+
+                	// flushing output
+                	output.flush();
+
+                	// closing streams
+                	output.close();
+                	input.close();
                 }
- 
-                // flushing output
-                output.flush();
- 
-                // closing streams
-                output.close();
-                input.close();
- 
+                else
+                {
+                	Log.i("UpdaterActivity", "O arquivo existe e possui tamanho igual ao arquivo remoto");
+                }
+
             } catch (Exception e) {
-                Log.e("Error: ", e.getMessage());
+            	Log.e("Error: ", e.getMessage());
             }
+            
  
             return null;
         }
@@ -155,17 +186,34 @@ public class Updater extends Activity {
          * */
         protected void onProgressUpdate(String... progress) {
             // setting progress percentage
-            pDialog.setProgress(Integer.parseInt(progress[0]));
+            if(progress[0].equals("progresso"))
+            {
+            	pDialog.setProgress(Integer.parseInt(progress[1]));
+            }
+            else 
+            {
+            	dismissDialog(progress_circle_type);
+            	showDialog(progress_bar_type);
+            	dialogType=1;
+            	
+            }
+            
        }
  
         /**
          * After completing background task
          * Dismiss the progress dialog
          * **/
-        @Override
+       	@Override
         protected void onPostExecute(String file_url) {
             // dismiss the dialog after the file was downloaded
-            dismissDialog(progress_bar_type);
+            if(dialogType == 0){
+            		dismissDialog(progress_circle_type);
+            }
+            else
+            {
+            	dismissDialog(progress_bar_type);
+            }
  
             /*AQUI É ONDE A MÁGICA COMEÇA! */
             
